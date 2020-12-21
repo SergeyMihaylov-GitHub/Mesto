495ea5437f8d1fde39fb3fca3167487699eae632
SergeyMihaylov-GitHub <da.outlook@outlook.com>
2020-12-21 21:03:08 +0300

    Final

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
 let blockPopup = document.querySelector('.popup');
-let openBlockPopup = document.querySelector('.profile__pic-edit');
-let closeBlockPopup = blockPopup.querySelector('.popup__close');
 let nameFromPage = document.querySelector('.profile__name');
 let jobFromPage = document.querySelector('.profile__profession');
 let nameFromForm = document.querySelector('.popup__input_name');
 let jobFromForm = document.querySelector('.popup__input_work');
+let openBlockPopup = document.querySelector('.profile__pic-edit');
+let closeBlockPopup = blockPopup.querySelector('.popup__close');
+
 
 function togglePopup(){
   blockPopup.classList.toggle('popup_opened');
   nameFromForm.value = nameFromPage.textContent;
   jobFromForm.value = jobFromPage.textContent;
-}
-
-function closePopup(){
-  blockPopup.classList.remove('popup_opened');
 }
 
 function savePopup (event){
@@ -23,6 +20,13 @@ function savePopup (event){
   closePopup();
 }
 
+function closePopup(){
+  blockPopup.classList.remove('popup_opened');
+}
+
 openBlockPopup.addEventListener('click', togglePopup);
-closeBlockPopup.addEventListener('click', togglePopup);
+
 blockPopup.addEventListener('submit', savePopup);
+
+closeBlockPopup.addEventListener('click', togglePopup);
+
